1e49594805e5ab58418ef3c0c156495642f71bd5
peacemo <carlchenxq@qq.com>
2023-04-10 16:40:30 +0800

    feat(main.py): ✨ 增加防御力
    执行前检查文件是否存在，防止误操作把之前的文档覆盖了。

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+import os
 import yaml
 from jinja2 import Template
 
 config_name = "example"
+
+if os.path.exists(f"./outputs/{config_name}.md"):
+    go_on = input(f"文件: ./outputs/{config_name}.md 已存在，继续执行将覆盖此文件[y/n]: ")
+    if go_on == "y" or go_on == "Y" or go_on == "yes":
+        pass
+    else: 
+        print("操作取消，程序退出。")
+        exit()
+
 # Load and parse the YAML file
 with open(f"./configs/{config_name}.yml") as f:
     data = yaml.safe_load(f)
